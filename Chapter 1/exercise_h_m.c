4d44c4004ecdec4071ef0c65b0eef74f0f2bc3db
supersaint7780 <hackerboy5556@gmail.com>
2021-11-28 16:52:06 +0530

    added the book pdf

---
+++ Chapter 1/exercise_h_m.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("enter a five digit number \n");
+    scanf("%d",&num);
+    printf("the required number is %d\n",(num+11111));
+    return 0;
+}
